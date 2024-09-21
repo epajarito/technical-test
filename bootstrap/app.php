3b337a3a54c1c993c559edbacd453ac512046c0a
Eduardo Pajarito <laalocuautle@gmail.com>
2024-09-20 23:25:31 -0600

    feat: register api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,7 +6,16 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
+        using: function (){
+            Route::middleware('web')
+                ->group(base_path('routes/web.php'));
+
+            Route::middleware('api')
+                ->prefix('api')
+                ->name('api.')
+                ->group(base_path('routes/api.php'));
+
+        },
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
